1663c64f30690eaf6237ca665b573ead2c95bc0a
Ali Ahmed <engr.aliahmed4@gmail.com>
2026-07-02 13:20:04 +0500

    Calculator practice done

--- Parsing-Req-NodeJS/calculator/sum.js
+++ Parsing-Req-NodeJS/calculator/sum.js
@@ -4,9 +4,11 @@ const sumRequestHandler = (req, res) => {
     const body = [];
     req.on("data", (chunk) => {
         body.push(chunk);
+        console.log(chunk); // Output: <Buffer 66 69 72 73 74 3d 33 26 73 65 63 6f 6e 64 3d 33>
     });
     req.on("end", () => {
         const bodyStr = Buffer.concat(body).toString();
+        console.log(bodyStr); // Now the Buffer class decrypted the chunk
         const params = new URLSearchParams(bodyStr);
         const bodyObj = Object.fromEntries(params);
         const result = Number(bodyObj.first) + Number(bodyObj.second);
