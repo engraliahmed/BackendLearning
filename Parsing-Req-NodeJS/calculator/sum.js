1d3b76165785628d3d2dea3e888798d32bddb8f7
Ali Ahmed <engr.aliahmed4@gmail.com>
2026-07-02 13:16:19 +0500

    Parsing Req Done

--- Parsing-Req-NodeJS/calculator/sum.js
+++ Parsing-Req-NodeJS/calculator/sum.js
@@ -3,15 +3,25 @@ const sumRequestHandler = (req, res) => {
 
     const body = [];
     req.on("data", (chunk) => {
-        body.push(chunk)
+        body.push(chunk);
     });
-    req.on('end', ()=>{
-        const bodyStr = Buffer.concat().toString()
-        const params = new URLSearchParams(bodyStr)
+    req.on("end", () => {
+        const bodyStr = Buffer.concat(body).toString();
+        const params = new URLSearchParams(bodyStr);
         const bodyObj = Object.fromEntries(params);
-        const result = bodyObj.first + bodyObj.second
-        
-    })
+        const result = Number(bodyObj.first) + Number(bodyObj.second);
+        console.log(result);
+        res.setHeader("Content-Type", "text/html");
+        res.write(`
+        <html>
+            <head><title>Practise Set</title></head>
+            <body>
+                <h1>Your Sum is ${result}</h1>
+            </body>  
+        <html>  
+    `);
+        return res.end();
+    });
 };
 
 module.exports = sumRequestHandler;
