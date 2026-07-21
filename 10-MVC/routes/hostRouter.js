f04591c1106414b733a535ace587f71eaabde55d
Ali Ahmed <engr.aliahmed4@gmail.com>
2026-07-22 00:24:45 +0500

    .

---
+++ 10-MVC/routes/hostRouter.js
@@ -0,0 +1,37 @@
+// Core Module
+const path = require("path");
+
+// External Module
+const express = require("express");
+const hostRouter = express.Router();
+
+// Local Module
+const rootDir = require("../utils/pathUtil");
+
+hostRouter.get("/add-home", (req, res, next) => {
+    // res.sendFile(path.join(rootDir, "views", "addHome.html"));
+    res.render("addHome", { pageTitle: "add home" });
+});
+
+const registeredHomes = [];
+
+hostRouter.post("/add-home", (req, res, next) => {
+    console.log(
+        "Home registration successful for: ",
+        req.body,
+        req.body.houseName,
+    );
+    registeredHomes.push({
+        houseName: req.body.houseName,
+        price: req.body.price,
+        date: req.body.date,
+        location: req.body.location,
+        image: req.body.image,
+    });
+    console.log(registeredHomes);
+    // res.sendFile(path.join(rootDir, "views", "homeAdded.html"));
+    res.render("homeAdded", { pageTitle: "Success" });
+});
+
+exports.hostRouter = hostRouter;
+exports.registeredHomes = registeredHomes;
