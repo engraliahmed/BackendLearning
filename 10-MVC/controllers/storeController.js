05ba2b04b84f3e5ffbbf8c61850085788f9d4229
Ali Ahmed <engr.aliahmed4@gmail.com>
2026-07-24 15:27:00 +0500

    Views Added, Logic Added in storeController

--- 10-MVC/controllers/storeController.js
+++ 10-MVC/controllers/storeController.js
@@ -4,6 +4,15 @@ exports.getHomes = (req, res, next) => {
     const registeredHomes = Home.fetchAll((registeredHomes) =>
         res.render("store/homepage", {
             registeredHomes,
+            pageTitle: "Home Page",
+        }),
+    );
+};
+
+exports.getIndex = (req, res, next) => {
+    const registeredHomes = Home.fetchAll((registeredHomes) =>
+        res.render("store/index", {
+            registeredHomes,
             pageTitle: "airbnb Home",
         }),
     );
@@ -14,3 +23,9 @@ exports.getBookings = (req, res, next) => {
         pageTitle: "My Bookings",
     });
 };
+
+exports.getFavouriteList = (req, res, next) =>{
+    res.render("store/favourites", {
+        pageTitle: "Favourites"
+    })
+}
--- 10-MVC/routes/storeRouter.js
+++ 10-MVC/routes/storeRouter.js
@@ -6,6 +6,8 @@ const storeRouter = express.Router();
 const storeController = require("../controllers/storeController");
 
 storeRouter.get("/", storeController.getHomes);
+storeRouter.get("/index", storeController.getIndex);
 storeRouter.get("/bookings", storeController.getBookings);
+storeRouter.get("/favourites", storeController.getFavouriteList);
 
 module.exports = storeRouter;
