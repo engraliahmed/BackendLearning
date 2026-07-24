df2f3720e58233d4f64eefe1ba71171fabc4a4b9
Ali Ahmed <engr.aliahmed4@gmail.com>
2026-07-24 16:22:09 +0500

    Host Homes Routes added

--- 10-MVC/controllers/hostController.js
+++ 10-MVC/controllers/hostController.js
@@ -4,12 +4,19 @@ exports.getAddHome = (req, res, next) => {
     res.render("host/addHome", { pageTitle: "add home" });
 };
 
+exports.getHostHomes = (req, res, next) => {
+    const registeredHomes = Home.fetchAll((registeredHomes) =>
+        res.render("host/hostHomeList", {
+            registeredHomes,
+            pageTitle: "Host Home List",
+        }),
+    );
+};
+
 exports.postAddHome = (req, res, next) => {
     const { houseName, price, date, location, image } = req.body;
-
     const home = new Home(houseName, price, date, location, image);
-
     home.save();
-
     res.render("host/homeAdded", { pageTitle: "Success" });
 };
+
