3be070a1ce1b017198c816e6b1a2e240680d1c68
Ali Ahmed <engr.aliahmed4@gmail.com>
2026-08-10 22:10:14 +0500

    Add login functionality with username input and update auth routes

--- 14-CookiesAndSession/controllers/authController.js
+++ 14-CookiesAndSession/controllers/authController.js
@@ -1,5 +1,11 @@
+const Home = require("../models/home");
+
 exports.getLogin = (req, res, next) => {
     res.render("auth/login", {
         pageTitle: "Login",
     });
+};
+
+exports.postLogin = (req, res, next) => {
+    res.redirect('/')
 };
